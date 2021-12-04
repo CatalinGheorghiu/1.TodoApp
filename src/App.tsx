9fe1033c7d226126facd63c6e320dbcc5f5c1e99
Ionut Catalin GHEORGHIU <ionutcatalingheorghiu@yahoo.com>
2021-12-04 20:23:38 +0200

    [CU-1wefb0c] Sketch a basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,28 @@ import React from 'react';
 
 const App = () => (
   <div className="App">
-    <h1>Vite + react + TS + eslint + prettier</h1>
+    <h1>Road to success: Todo App</h1>
+
+    <label>
+      <input type="text" placeholder="Add todo" />
+    </label>
+    <button type="button">Add Todo</button>
+    <div className="todos">
+      <ul>
+        <li>
+          <p>Todo 1</p>
+          <button>❌</button>
+        </li>
+        <li>
+          <p>Todo 2</p>
+          <button>❌</button>
+        </li>
+        <li>
+          <p>Todo 3</p>
+          <button>❌</button>
+        </li>
+      </ul>
+    </div>
   </div>
 );
 
