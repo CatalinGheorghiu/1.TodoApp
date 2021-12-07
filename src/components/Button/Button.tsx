e01f03c256f9109a4c5629232f150ff0284592e5
Ionut Catalin GHEORGHIU <ionutcatalingheorghiu@yahoo.com>
2021-12-07 19:12:15 +0200

    CU-1wegja5[review] - Add the functionality for creating a todo

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,14 +4,21 @@ import styles from './Button.module.scss';
 
 interface ButtonProps {
   buttonType: 'button' | 'submit' | 'reset';
+  buttonClick: () => void;
   buttonText: string;
   buttonClassName?: string;
 }
 
-const Button = ({ buttonType, buttonText, buttonClassName }: ButtonProps) => (
+const Button = ({
+  buttonType,
+  buttonText,
+  buttonClick,
+  buttonClassName,
+}: ButtonProps) => (
   <button
     className={buttonClassName && styles[buttonClassName]}
-    type={buttonType}>
+    type={buttonType}
+    onClick={buttonClick}>
     {buttonText}
   </button>
 );
